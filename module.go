8fc5e782184fffe77a626d0d77a6a969340c51db
MEZ <mez@bitwormhole.com>
2023-07-28 14:25:58 +0800

    up to v1.0.5

--- module.go
+++ module.go
@@ -10,8 +10,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/security"
-	theModuleVersion  = "v1.0.4"
-	theModuleRevision = 6
+	theModuleVersion  = "v1.0.5"
+	theModuleRevision = 7
 	theModuleResPath  = "src/main/resources"
 )
 
