9677057e083541d683629600d2a8618c55d1c04f
MEZ <mez@bitwormhole.com>
2023-10-21 09:25:46 +0800

    update to v1.0.25

--- module_t.go
+++ module_t.go
@@ -9,8 +9,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/security"
-	theModuleVersion  = "v1.0.24"
-	theModuleRevision = 26
+	theModuleVersion  = "v1.0.25"
+	theModuleRevision = 27
 	theModuleResPath  = "src/main/resources"
 )
 
