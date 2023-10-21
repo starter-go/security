6f57c68140f1ad10c28cbe3a1b05b969d5bea467
MEZ <mez@bitwormhole.com>
2023-10-21 09:25:20 +0800

    改进了 security.Subject 的实现

--- subject.go
+++ subject.go
@@ -12,11 +12,32 @@ const theSubjectBindingKey = "rbac.Subject#binding"
 
 // Subject 代表当前主体
 type Subject interface {
-	Init(ss SessionService)
+	// Init(ss SessionService)
 
 	GetSession(create bool) Session
 
 	HasRole(role rbac.RoleName) bool
+}
+
+// SetupSubject 把主体绑定到上下文中
+func SetupSubject(c context.Context, ss SessionService) (Subject, error) {
+
+	sub, err := GetSubject(c)
+	if err == nil && sub != nil {
+		return sub, nil
+	}
+
+	const key = theSubjectBindingKey
+	kv, err := context2.GetValues(c)
+	if err != nil {
+		return nil, err
+	}
+
+	// make new subject
+	s2 := &subject{context: c, sessionService: ss}
+	sub = s2
+	kv.SetValue(key, sub)
+	return sub, nil
 }
 
 // GetSubject 从上下文中提取当前主体
@@ -27,17 +48,9 @@ func GetSubject(c context.Context) (Subject, error) {
 		return nil, err
 	}
 	sub, ok := kv.GetValue(key).(Subject)
-	if ok && sub != nil {
+	if !ok {
 		return sub, nil
 	}
-
-	// make new subject
-	s2 := &subject{
-		context: c,
-	}
-
-	sub = s2
-	kv.SetValue(key, sub)
 	return sub, nil
 }
 
