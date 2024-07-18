32f397c9670c34290856a24341d513c64dfd2a6f
xukun <xukun17@sina.com>
2024-07-18 15:40:26 +0800

    up to v1.0.55

--- module_t.go
+++ module_t.go
@@ -8,8 +8,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/security"
-	theModuleVersion  = "v1.0.54.4"
-	theModuleRevision = 64
+	theModuleVersion  = "v1.0.55"
+	theModuleRevision = 65
 )
 
 ////////////////////////////////////////////////////////////////////////////////
