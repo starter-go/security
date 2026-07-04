121d0c9af44557c15495d51d4b1d67300dd8f8b8
kge <kge@bitwormhole.com>
2026-07-04 19:58:12 +0800

    up to v0.1.58

--- module_t.go
+++ module_t.go
@@ -8,8 +8,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/security"
-	theModuleVersion  = "v1.0.57"
-	theModuleRevision = 67
+	theModuleVersion  = "v0.1.58"
+	theModuleRevision = 68
 )
 
 ////////////////////////////////////////////////////////////////////////////////
